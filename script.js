942ea036c335562cbb35b3b8f3d6a220e341c40b
michael houze <michael-houze@loyalhealth.com>
2023-11-07 08:04:01 -0500

    cleaned up timing and repeat logic. added countdown.

--- script.js
+++ script.js
@@ -1,62 +1,109 @@
+"use strict";
 // TODO: add responsive css
-// TODO: reduce the chance of back to back duplicate words
-// TODO: hardcode colors
-var intervalId;
-var counts = {};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let intervalId;
+let counts = {};
+let lastWordSpoken = '';
+let wordRepeated = false;
+let spokenWords = 0;
 function startExercise() {
-    // Reset counts for each word
-    counts = {};
-    var wordInput = document.getElementById('wordArray').value;
-    var minTimeInterval = parseInt(document.getElementById('minTimeInterval').value) * 1000;
-    var maxTimeInterval = parseInt(document.getElementById('maxTimeInterval').value) * 1000;
-    var wordCount = parseInt(document.getElementById('wordCount').value);
-    var spokenWords = 0;
-    if (minTimeInterval > maxTimeInterval) {
-        alert('Minimum time interval must be less than maximum time interval.');
-        return;
-    }
-    var wordArray = wordInput.split(/[\n\r\s]+/).filter(Boolean).slice(0, 10);
-    var speakRandomWord = function () {
-        var wordIndex = Math.floor(Math.random() * wordArray.length);
-        var word = wordArray[wordIndex];
-        counts[word] = counts[word] ? counts[word] + 1 : 1;
-        spokenWords++;
-        if (spokenWords <= wordCount) {
-            // Using the Web Speech API to speak the word
-            var utterance = new SpeechSynthesisUtterance(word);
-            console.log("word: ".concat(word));
-            console.log("word count: ".concat(spokenWords));
-            speechSynthesis.speak(utterance);
-            // Check if we've said the word the required number of times
-            if (spokenWords < wordCount) {
-                scheduleNextWord();
-            }
-            else {
-                setTimeout(function () {
-                    console.log('This will log after 2 seconds.');
-                }, 2000);
-                var completeUtterance = new SpeechSynthesisUtterance('exercise complete');
-                speechSynthesis.speak(completeUtterance);
-                console.log("Finished saying ".concat(word, " ").concat(wordCount, " times."));
-            }
+    return __awaiter(this, void 0, void 0, function* () {
+        // Reset counts for each word
+        counts = {};
+        const selectElement = document.getElementById("wordSelect");
+        const wordCount = parseInt(document.getElementById("wordCount").value);
+        const minTimeInterval = parseInt(document.getElementById("minTimeInterval").value) * 1000;
+        const maxTimeInterval = parseInt(document.getElementById("maxTimeInterval").value) * 1000;
+        if (minTimeInterval > maxTimeInterval) {
+            alert("Minimum time interval must be less than maximum time interval.");
+            return;
         }
-    };
-    var scheduleNextWord = function () {
-        var timeInterval = getRandomTimeInterval(minTimeInterval, maxTimeInterval);
-        intervalId = window.setTimeout(speakRandomWord, timeInterval);
-    };
-    // Start the process
-    scheduleNextWord();
+        const selectedOptions = Array.from(selectElement.selectedOptions).map((option) => option.value);
+        const wordArray = selectedOptions.length > 0
+            ? selectedOptions
+            : ["green", "yellow", "red", "orange"]; // Fallback to all words if none selected
+        yield countdown();
+        const speakRandomWord = () => __awaiter(this, void 0, void 0, function* () {
+            let word = '';
+            if ((Math.random() >= 0.1) || wordRepeated) { // must be a new word
+                do {
+                    var wordIndex = Math.floor(Math.random() * wordArray.length);
+                    word = wordArray[wordIndex];
+                } while ((word === lastWordSpoken) && (wordArray.length > 1));
+                wordRepeated = false;
+            }
+            else { // repeat same word
+                word = lastWordSpoken;
+                wordRepeated = true;
+            }
+            //counts[word] = counts[word] ? counts[word] + 1 : 1;
+            spokenWords++;
+            if (spokenWords <= wordCount) {
+                // Using the Web Speech API to speak the word
+                var utterance = new SpeechSynthesisUtterance(word);
+                lastWordSpoken = word;
+                console.log(`word: ${word}`);
+                console.log(`word count: ${spokenWords}`);
+                speechSynthesis.speak(utterance);
+                // Check if we've said the word the required number of times
+                if (spokenWords < wordCount) {
+                    scheduleNextWord();
+                }
+                else {
+                    yield delay(3000);
+                    var completeUtterance = new SpeechSynthesisUtterance("Exercise complete. Great Job!");
+                    speechSynthesis.speak(completeUtterance);
+                    console.log(`Finished saying ${wordCount} words.`);
+                }
+            }
+        });
+        const scheduleNextWord = () => {
+            var timeInterval = getRandomTimeInterval(minTimeInterval, maxTimeInterval);
+            intervalId = window.setTimeout(speakRandomWord, timeInterval);
+        };
+        // Start the process
+        scheduleNextWord();
+    });
+}
+function delay(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+function countdown() {
+    return __awaiter(this, void 0, void 0, function* () {
+        var startUtterance = new SpeechSynthesisUtterance("Starting exercise. Beginning in three.");
+        speechSynthesis.speak(startUtterance);
+        yield delay(3000);
+        var twoUtterance = new SpeechSynthesisUtterance("two");
+        speechSynthesis.speak(twoUtterance);
+        yield delay(1000);
+        var oneUtterance = new SpeechSynthesisUtterance("one");
+        speechSynthesis.speak(oneUtterance);
+        return new Promise(resolve => setTimeout(resolve, 2000));
+    });
 }
 function stopExercise() {
-    if (intervalId !== undefined) {
-        clearTimeout(intervalId);
-        intervalId = undefined;
-        speechSynthesis.cancel();
-    }
-    console.log("Stopped and reset.");
+    return __awaiter(this, void 0, void 0, function* () {
+        if (intervalId !== undefined) {
+            clearTimeout(intervalId);
+            intervalId = undefined;
+            speechSynthesis.cancel();
+            spokenWords = 0;
+        }
+        console.log("Stopped and reset.");
+    });
 }
 function getRandomTimeInterval(min, max) {
+    if (spokenWords == 0) {
+        return 1;
+    }
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 // For demonstration purposes, attaching functions to window to make them accessible from HTML
--- script.ts
+++ script.ts
@@ -1,56 +1,93 @@
 // TODO: add responsive css
-// TODO: reduce the chance of back to back duplicate words
-// TODO: hardcode colors
 
 let intervalId: number | undefined;
 let counts: Record<string, number> = {};
+let lastWordSpoken: string = '';
+let wordRepeated: boolean = false;
+let spokenWords: number = 0;
 
-function startExercise() {
+async function startExercise() {
   // Reset counts for each word
   counts = {};
 
-  const wordInput = (document.getElementById('wordArray') as HTMLTextAreaElement).value;
-  const minTimeInterval = parseInt((document.getElementById('minTimeInterval') as HTMLInputElement).value) * 1000;
-  const maxTimeInterval = parseInt((document.getElementById('maxTimeInterval') as HTMLInputElement).value) * 1000;
-  const wordCount = parseInt((document.getElementById('wordCount') as HTMLInputElement).value);
-  var spokenWords = 0;
+
+  const selectElement = document.getElementById(
+    "wordSelect"
+  ) as HTMLSelectElement;
+  const wordCount = parseInt(
+    (document.getElementById("wordCount") as HTMLInputElement).value
+  );
+  const minTimeInterval =
+    parseInt(
+      (document.getElementById("minTimeInterval") as HTMLInputElement).value
+    ) * 1000;
+  const maxTimeInterval =
+    parseInt(
+      (document.getElementById("maxTimeInterval") as HTMLInputElement).value
+    ) * 1000;
 
   if (minTimeInterval > maxTimeInterval) {
-    alert('Minimum time interval must be less than maximum time interval.');
+    alert("Minimum time interval must be less than maximum time interval.");
     return;
   }
 
-  const wordArray = wordInput.split(/[\n\r\s]+/).filter(Boolean).slice(0, 10);
+  const selectedOptions = Array.from(selectElement.selectedOptions).map(
+    (option) => option.value
+  );
+  
+  const wordArray =
+    selectedOptions.length > 0
+      ? selectedOptions
+      : ["green", "yellow", "red", "orange"]; // Fallback to all words if none selected
 
-  const speakRandomWord = () => {
-    const wordIndex = Math.floor(Math.random() * wordArray.length);
-    const word = wordArray[wordIndex];
-    counts[word] = counts[word] ? counts[word] + 1 : 1;
+  await countdown();
+
+  const speakRandomWord = async () => {
+    let word: string = '';
+
+    if ((Math.random() >= 0.1) || wordRepeated) { // must be a new word
+      do {
+        var wordIndex = Math.floor(Math.random() * wordArray.length);
+        word = wordArray[wordIndex];
+      } while ((word === lastWordSpoken) && (wordArray.length > 1));
+
+      wordRepeated = false;
+    } 
+    else { // repeat same word
+      word = lastWordSpoken;
+      wordRepeated = true;
+    }
+    
+    //counts[word] = counts[word] ? counts[word] + 1 : 1;
     spokenWords++;
 
     if (spokenWords <= wordCount) {
       // Using the Web Speech API to speak the word
       var utterance = new SpeechSynthesisUtterance(word);
-      console.log(`word: ${word}`)
-      console.log(`word count: ${spokenWords}`)
+      lastWordSpoken = word;
+      console.log(`word: ${word}`);
+      console.log(`word count: ${spokenWords}`);
       speechSynthesis.speak(utterance);
 
       // Check if we've said the word the required number of times
       if (spokenWords < wordCount) {
         scheduleNextWord();
       } else {
-        setTimeout(() => {
-          console.log('This will log after 2 seconds.');
-        }, 2000);
-        var completeUtterance = new SpeechSynthesisUtterance('exercise complete');
+        await delay(3000);
+        var completeUtterance = new SpeechSynthesisUtterance(
+          "Exercise complete. Great Job!"
+        );
         speechSynthesis.speak(completeUtterance);
-        console.log(`Finished saying ${word} ${wordCount} times.`);
+        console.log(`Finished saying ${wordCount} words.`);
       }
     }
   };
 
   const scheduleNextWord = () => {
-    const timeInterval = getRandomTimeInterval(minTimeInterval, maxTimeInterval);
+    var timeInterval = getRandomTimeInterval(
+      minTimeInterval,
+      maxTimeInterval
+    );
     intervalId = window.setTimeout(speakRandomWord, timeInterval);
   };
 
@@ -58,16 +95,46 @@ function startExercise() {
   scheduleNextWord();
 }
 
-function stopExercise() {
+function delay(milliseconds: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function countdown(): Promise<void> {
+  var startUtterance = new SpeechSynthesisUtterance(
+    "Starting exercise. Beginning in three."
+  );
+  speechSynthesis.speak(startUtterance);
+  await delay(3000);
+
+  var twoUtterance = new SpeechSynthesisUtterance(
+    "two"
+  );
+  speechSynthesis.speak(twoUtterance);
+  await delay(1000);
+
+  var oneUtterance = new SpeechSynthesisUtterance(
+    "one"
+  );
+  speechSynthesis.speak(oneUtterance);
+
+  return new Promise(resolve => setTimeout(resolve, 2000));
+}
+
+async function stopExercise() {
   if (intervalId !== undefined) {
     clearTimeout(intervalId);
     intervalId = undefined;
     speechSynthesis.cancel();
+    spokenWords = 0;
   }
   console.log("Stopped and reset.");
 }
 
 function getRandomTimeInterval(min: number, max: number): number {
+  if(spokenWords == 0){
+    return 1;
+  }
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
